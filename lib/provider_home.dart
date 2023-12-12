9bf514434383f7fe6fba49a74cf45432f7b9efcf
Waqas Sharif <119389797+waqassharif848@users.noreply.github.com>
2023-12-12 14:20:52 +0500

    state change

--- lib/provider_home.dart
+++ lib/provider_home.dart
@@ -1,51 +1,67 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:provide/provider/count_provider.dart';
 import 'package:provider/provider.dart';
 
-
-
-
-class ProviderHome extends StatefulWidget {
-  const ProviderHome({super.key});
+class ProviderHome extends StatelessWidget {
+  const ProviderHome({Key? key}) : super(key: key);
 
   @override
-  State<ProviderHome> createState() => _ProviderHomeState();
+  Widget build(BuildContext context) {
+    return _ProviderHomeContent();
+  }
 }
 
-class _ProviderHomeState extends State<ProviderHome> {
+class _ProviderHomeContent extends StatefulWidget {
+  const _ProviderHomeContent({Key? key}) : super(key: key);
+
+  @override
+  State<_ProviderHomeContent> createState() => _ProviderHomeContentState();
+}
+
+class _ProviderHomeContentState extends State<_ProviderHomeContent> {
+  late Timer _timer;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     final countProvider = Provider.of<CountProvider>(context, listen: false);
-    Timer.periodic(Duration(minutes: 1), (timer) {
+    _timer = Timer.periodic(Duration(minutes: 1), (timer) {
       countProvider.setCount();
     });
   }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final countProvider = Provider.of<CountProvider>(context, listen: false);
-    print('build');
+
     return Scaffold(
-  appBar: AppBar(
-    title: Text('Provider_1'),
-  ),
-  body:Center(
-    child: Consumer<CountProvider>(
-      builder: (context, value, child){
-        print('Just this widget build');
-        return Text(countProvider.count.toString(),
-          style: TextStyle(fontSize: 40),);
-      }),
-  ) ,
-  floatingActionButton: FloatingActionButton(
-    onPressed: () {
-      countProvider.setCount();
-    },
-    child: Icon(Icons.add),
-  ),
-);
+      appBar: AppBar(
+        title: Text('Provider_1'),
+      ),
+      body: Center(
+        child: Consumer<CountProvider>(
+          builder: (context, value, child) {
+            print('Just this widget build');
+            return Text(
+              value.count.toString(),
+              style: TextStyle(fontSize: 40),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          countProvider.setCount();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
